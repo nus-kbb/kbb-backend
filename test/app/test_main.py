41643ffcc51dae795baeac230b4b68362b610ae3
Terry Chia <chiawz@hotmail.com>
2023-03-19 17:16:05 +0800

    fix[NK-31]: fix unit tests

--- test/app/test_main.py
+++
@@ -1,4 +0,0 @@
-class TestClass:
-    def test_one(self):
-        x = "this"
-        assert "this" in x
---
+++ test_main.py
@@ -0,0 +1,8 @@
+import pytest
+from main import app
+
+def test_index_route():
+    response = app.test_client().get('/home')
+
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Serving Hello world!'
