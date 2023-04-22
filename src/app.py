9031b391d39caae5a7d5a1092b532124c2036253
Kenneth Zhu <kenneth.zhu1988@gmail.com>
2023-04-22 21:11:03 +0800

    sample create project api

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
 from .controller.task_controller.task_dto.task_dto import Task
 from .controller.task_controller.task_controller import TaskController
 from .http_status_code_enum import HttpCode
@@ -36,6 +36,28 @@ def get_all_task_handler():
     taskList = taskController.GetAllTask()
     jsonTaskList = json.dumps(taskList, indent = 8)
     return jsonTaskList, HttpCode.Success.value, {'Content-Type': 'application/json'}
+
+
+# A dictionary to store the projects
+projects = {}
+
+# Endpoint to create a new project
+@app.route('/create_project', methods=['POST'])
+def create_project():
+    project_data = request.get_json()
+
+    # Check if the project name is already taken
+    if project_data['name'] in projects:
+        return jsonify({'error': 'Project name already taken.'}), 400
+
+    # Create the new project
+    projects[project_data['name']] = {
+        'name': project_data['name'],
+        'description': project_data['description'],
+        'tasks': []
+    }
+
+    return jsonify({'message': 'Project created successfully.'}), 201
 
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port=3000)
