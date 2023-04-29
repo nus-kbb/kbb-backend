cff29e58e3d95b8688a2a5981dfcea5706f64016
Song Weiyang <weiyang.song@shopee.com>
2023-04-29 12:52:33 +0800

    add support for cors

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from src.dto.task_dto.task_dto import Task
 from src.controller.task_controller.task_controller import TaskController
 from src.controller.user_controller.user_controller import UserController
@@ -8,6 +9,7 @@ import json
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
+CORS(app)
 taskController = TaskController()
 userController = UserController()
 
